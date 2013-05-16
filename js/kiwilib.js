205a95a0e8ffc72918e31bc16356e4212d088da2
Johnny Zabonik <jzabonik@mymail.mines.edu>
2013-05-16 09:58:29 -0600

    Johnny/Adam changes

--- js/kiwilib.js
+++ js/kiwilib.js
@@ -22,10 +22,10 @@ function refreshCanvas() {
     // For each id in layerList, call this function:
     $.each(layerList, function(i, id) {
         // Get the dot for this layer
-        var dot = objectList[id];
+        var dObj = objectList[id];
 
         // Draw the dot
-        dot.draw(ctx);
+        dObj.draw(ctx);
     });
 }
 
@@ -83,54 +83,14 @@ function startLine(e) {
         var last = this.pts[0];
         for(var i=1; i<this.pts.length; i++) {
             ctx.lineTo(this.pts[i][0], this.pts[i][1]);
+			ctx.lineJoin = 'round';
+			ctx.lineCap = 'round';
 			ctx.lineWidth = 10;
         };
         ctx.stroke();
     };
     assignID(line);
-    refreshCanvas();
-}
-
-function continueLine(e) {
-    var line = objectList[layerList[layerList.length-1]];
-    line.pts.push([e.pageX, e.pageY]);
-    refreshCanvas();
-}
-
-// Create a new dot using the event e
-function createDot(e) {
-
-    // Initialize a 'dot'; a dot is a hash with two parts: an (x,y) coordinate, and a draw function.
-    var dot = {
-        x:e.pageX,
-        y:e.pageY
-    };
-
-    // Set the dot's draw function.
-
-    // This is actually really important; every object that gets drawn MUST have a draw function.
-    // This is because RefreshCanvas() loops through all the objects, and calls all of their
-    // draw functions. If it doesn't have a draw function, the script breaks.
-
-    // The draw function is NOT called here, it's just defined. It will get called later.
-    dot.draw = function(ctx) {
-        // Begin a 'path'. A path tells the canvas where to draw or fill.
-        ctx.beginPath();
-
-        // Make an arc centered at x and y with radius 4 that goes from angle 0 to angle 2*PI
-        ctx.arc(this.x-2, this.y-2, 4, 0, 2*Math.PI);
-
-        // Draw the arc.
-        ctx.stroke();
-    };
-
-    // Give this dot an ID.
-    assignID(dot);
-
-    // We just made a dot, so let's make an action for it so we can undo it later.
-
-    // An 'action' is a hash which MUST contain two functions: undo and redo. These will get called later.
-    var newAct = {
+	var newAct = {
         undo: function() {
             // Take the top layer off of layerList. The object still exists in the objects hash, but
             // doesn't get drawn because ONLY the objects in layerList get drawn.
@@ -144,6 +104,12 @@ function createDot(e) {
 
     // Add the new action and redraw.
     addAction(newAct);
+    refreshCanvas();
+}
+
+function continueLine(e) {
+    var line = objectList[layerList[layerList.length-1]];
+    line.pts.push([e.pageX, e.pageY]);
     refreshCanvas();
 }
 
