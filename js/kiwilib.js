ed7f675133f8e436a075364bb7c5f743d1e80832
max <max@Lyla.(none)>
2013-05-17 10:50:46 -0600

    AUGH

--- js/kiwilib.js
+++ js/kiwilib.js
@@ -29,8 +29,8 @@ function refreshCanvas() {
             break;
     }
 
-    ctx.canvas.width  = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
+//    ctx.canvas.width  = window.innerWidth;
+//    ctx.canvas.height = window.innerHeight;
 
     // Set the fill color and fill the background
     ctx.fillStyle="#FFFFFF";
