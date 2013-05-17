3bba70f6fda77f7be06b011c55309c749cf43625
max <max@Lyla.(none)>
2013-05-17 11:03:05 -0600

    Falafal

--- js/kiwilib.js
+++ js/kiwilib.js
@@ -34,9 +34,8 @@ function refreshCanvas() {
             break;
     }
 
-    ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
     ctx.fillStyle="#FFFFFF";
-
+    ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
 
     // For each id in layerList, call this function:
     $.each(layerList, function(i, id) {
