81b3a8ca378a159356b34b98a672cd800ed92c37
Adam Bodnar <abodnar@mines.edu>
2013-05-23 14:11:05 -0600

    implemented a functional spray tool

--- js/kiwilib.js
+++ js/kiwilib.js
@@ -267,13 +267,13 @@ function pointerDown(e) {
             }
             break;
 
-        case "fill":
+		case "fill":
 			var dObj = {
 				color: curColor,
 				pts: [x, y]
 			}
 			createFill(dObj);
-            break;
+			break;
 
         case "stamp":
 			var dObj = {
@@ -348,6 +348,9 @@ function pointerMove(e) {
         switch(curTool) {
             case "draw":
                 continueLine(x,y);
+                break;
+			case "spray":
+                continueSpray(x,y);
                 break;
             case "erase":
                 var id = getObjectID(x,y);
@@ -459,8 +462,6 @@ function createFill(dObj){
 			ctx.putImageData(img,0,0);
 		ctx.restore();
 	};
-
-
 
     var newAct = {
         undo: function() {
@@ -602,98 +603,111 @@ function createPencilTex(dObj){
 		}
 		dObj.pattern =  dc.createPattern(texcanvas, "repeat");
 	}
-	else{
-		var w = dObj.width;
-		var grd=dc.createRadialGradient(dObj.pts[dObj.pts.length-1][0],dObj.pts[dObj.pts.length-1][1],w/8.0,dObj.pts[dObj.pts.length-1][0],dObj.pts[dObj.pts.length-1][1],w/2.0);
-		grd.addColorStop(0,dObj.color);
-		grd.addColorStop(1, "blue");
-//		dc.arc(w/2.0,w/2.0,w/2.0,0,2*Math.PI);
-//		dc.fillStyle = grd;
-//		dc.fill();
-		dObj.pattern =  grd;
-	}
 	dc.globalAlpha = 1;
 
 }
-
-function spraycanLine(dObj){	/////////////////testing spraycan
-	var texcanvas = document.createElement('canvas');
-	var dc = texcanvas.getContext('2d');
+function createSpraytex(dObj){
+	var scanvas = document.createElement('canvas');
+	scanvas.height = scanvas.width = dObj.width;
+	var ctx = scanvas.getContext('2d');
 	var w = dObj.width;
-	var grd=dc.createRadialGradient(dObj.pts[dObj.pts.length-1][0],dObj.pts[dObj.pts.length-1][1],w/8.0,dObj.pts[dObj.pts.length-1][0],dObj.pts[dObj.pts.length-1][1],w/2.0);
+	var grd=ctx.createRadialGradient(w/2,w/2,0,w/2,w/2,w/2);
 	grd.addColorStop(0,dObj.color);
-	grd.addColorStop(1, "rgba(255,255,255,0)");
-//		dc.arc(w/2.0,w/2.0,w/2.0,0,2*Math.PI);
-//		dc.fillStyle = grd;
-//		dc.fill();
-	dObj.pattern =  grd;
+	grd.addColorStop(1, "rgba(255,0,0,0)");
+	//dObj.grd = grd;
+	ctx.fillStyle = grd;
+	var w = dObj.width;
+	ctx.fillRect(0,0,w,w);
+	dObj.scanvas = scanvas;
 }
+
 
 function startLine(dObj) {
     assignID(dObj);
-
+	if(dObj.type == 'spray'){
+		createSpraytex(dObj);
+	}
 	// create brush pattern
 	if(dObj.type == 'graphite'){
 		createPencilTex(dObj);
 	}
-	if(dObj.type == 'spray'){//////////// testing spraycan
-		spraycanLine(dObj);
+	if(brushMode == 'simple' || brushMode == 'graphite'){
+		dObj.draw = function(ctx) {
+			ctx.save();
+			ctx.translate(this.mx,this.my);
+			ctx.scale(this.xScale, this.yScale);
+			ctx.rotate(-this.rotation);
+
+			ctx.beginPath();
+			ctx.moveTo(this.pts[0][0]-this.mx, this.pts[0][1]-this.my);
+
+			ctx.strokeStyle = this.color;
+			if(this.type == 'graphite'){
+				ctx.strokeStyle = this.pattern;
+			}
+				
+			ctx.fillStyle = this.color;
+			ctx.lineJoin = 'round';
+			ctx.lineCap = 'round';
+			ctx.lineWidth = this.width;
+			ctx.globalAlpha = this.opacity;
+
+			if(this.pts.length == 1) {
+				ctx.fillStyle = this.color;
+				if(this.type == 'graphite') {
+					ctx.fillStyle = this.pattern;
+				}
+				ctx.lineWidth = 0;
+				ctx.arc(this.pts[0][0]-this.mx, this.pts[0][1]-this.my, this.width/2, 0, 2*Math.PI);
+				ctx.fill();
+			} else if(!this.bezier) {
+
+				// Draw the line without beziers
+				for(var i=1; i<this.pts.length; i++) {
+
+					ctx.lineTo(this.pts[i][0]-this.mx, this.pts[i][1]-this.my);
+				};
+				ctx.stroke();
+			} else {
+
+				// Draw the line with beziers
+				for(var i=0; i<this.pts.length; i+=3) {
+					if(this.pts.length <= i+4) {
+						for(var j=i; j<this.pts.length; j++) {
+							ctx.lineTo(this.pts[j][0]-this.mx,this.pts[j][1]-this.my);
+						}
+					} else {
+						ctx.bezierCurveTo(this.pts[i+1][0]-this.mx, this.pts[i+1][1]-this.my,
+							this.pts[i+2][0]-this.mx, this.pts[i+2][1]-this.my,
+							this.pts[i+3][0]-this.mx, this.pts[i+3][1]-this.my);
+					}
+				};
+				ctx.stroke();
+			}
+			ctx.restore();
+		};
 	}
-	
-    dObj.draw = function(ctx) {
-        ctx.save();
-        ctx.translate(this.mx,this.my);
-        ctx.scale(this.xScale, this.yScale);
-        ctx.rotate(-this.rotation);
-
-        ctx.beginPath();
-        ctx.moveTo(this.pts[0][0]-this.mx, this.pts[0][1]-this.my);
-
-	ctx.strokeStyle = this.color;
-	if(this.type == 'graphite' || this.type == 'spray'){
-	    ctx.strokeStyle = this.pattern;
+	else if(brushMode == 'spray'){
+		dObj.draw = function(ctx) {
+		ctx.save();
+			ctx.translate(this.mx,this.my);
+			ctx.scale(this.xScale, this.yScale);
+			ctx.rotate(-this.rotation);
+			ctx.beginPath();
+			ctx.moveTo(this.pts[0][0]-this.mx, this.pts[0][1]-this.my);
+			ctx.lineWidth = this.width;
+			ctx.globalAlpha = this.opacity;
+			var w = dObj.width;
+			for(var i=1; i<this.pts.length; i++) {
+				//for(var j=0; j<5; j++){
+				ctx.drawImage(dObj.scanvas,this.pts[i][0]-this.mx-w/2, this.pts[i][1]-this.my-w/2);
+				//ctx.lineTo(this.pts[i][0]-this.mx, this.pts[i][1]-this.my);
+				//}
+			};
+			ctx.stroke();
+		ctx.restore();
+		}
 	}
-		
-        ctx.fillStyle = this.color;
-        ctx.lineJoin = 'round';
-        ctx.lineCap = 'round';
-        ctx.lineWidth = this.width;
-        ctx.globalAlpha = this.opacity;
-
-        if(this.pts.length == 1) {
-            ctx.fillStyle = this.color;
-            if(this.type == 'graphite' || this.type == 'spray') {
-                ctx.fillStyle = this.pattern;
-            }
-            ctx.lineWidth = 0;
-            ctx.arc(this.pts[0][0]-this.mx, this.pts[0][1]-this.my, this.width/2, 0, 2*Math.PI);
-            ctx.fill();
-        } else if(!this.bezier) {
-
-            // Draw the line without beziers
-            for(var i=1; i<this.pts.length; i++) {
-
-                ctx.lineTo(this.pts[i][0]-this.mx, this.pts[i][1]-this.my);
-            };
-            ctx.stroke();
-        } else {
-
-            // Draw the line with beziers
-            for(var i=0; i<this.pts.length; i+=3) {
-                if(this.pts.length <= i+4) {
-                    for(var j=i; j<this.pts.length; j++) {
-                        ctx.lineTo(this.pts[j][0]-this.mx,this.pts[j][1]-this.my);
-                    }
-                } else {
-                    ctx.bezierCurveTo(this.pts[i+1][0]-this.mx, this.pts[i+1][1]-this.my,
-                        this.pts[i+2][0]-this.mx, this.pts[i+2][1]-this.my,
-                        this.pts[i+3][0]-this.mx, this.pts[i+3][1]-this.my);
-                }
-            };
-            ctx.stroke();
-        }
-        ctx.restore();
-    };
     dObj.drawIcons = function(ctx) {
         var leftCorner = transformPoint(
             this.lCorner[0]-this.mx-32, this.lCorner[1]-this.my-32,
@@ -833,10 +847,6 @@ function continueLine(x,y) {
     if(y > dObj.rCorner[1]) { dObj.rCorner[1] = y; }
     dObj.mx = (dObj.lCorner[0]+dObj.rCorner[0])/2;
     dObj.my = (dObj.lCorner[1]+dObj.rCorner[1])/2;
-
-	if (dObj.type == 'spray'){/////////////////////// testing spraycan
-		spraycanLine(dObj);
-	}
 
 }
 
