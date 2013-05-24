f48a275937e4a5f81236b7691b1835547ad489b8
max <max@Lyla.(none)>
2013-05-24 08:46:02 -0600

    All cleanup finished.

--- js/kiwilib.js
+++ js/kiwilib.js
@@ -15,16 +15,6 @@ var curStamp = '';
 var curZoom = 1;
 var scratch;
 
-var selectedId = -1;
-var xOld;
-var yOld;
-var xFirst;
-var yFirst;
-var dragMode = '';
-var shift = false;
-var xScaleOld;
-var yScaleOld;
-
 var tx=0;
 var ty=0;
 var orientation = orienting() ? window.orientation : 0;
@@ -244,16 +234,7 @@ function pointerDown(e) {
                     dragMode = objectList[selectedId].iconClicked(x, y);
                     isDragging = true;
                     if(dragMode == 'scale') {
-                        xScaleOld = objectList[selectedId].xScale;
-                        yScaleOld = objectList[selectedId].yScale;
-                        var mx = objectList[selectedId].midX();
-                        var my = objectList[selectedId].midY();
-                        var pt = transformPoint(x-mx,y-my,
-                            mx, my,
-                            1, 1,
-                            objectList[selectedId].rotation);
-                        xOld = pt[0]; yOld = pt[1];
-                        xFirst = pt[0]; yFirst = pt[1];
+                        beginScale(selectedId, x, y);
                     }
                 } else {
                     selectedId = getObjectID(x, y);
@@ -267,9 +248,7 @@ function pointerDown(e) {
             case "erase":
                 isDragging = true;
                 var id = getObjectID(x,y);
-                if(id != -1) {
-                    eraseObject(id);
-                }
+                if(id != -1) { eraseObject(id); }
                 break;
 
             case "fill":
@@ -314,52 +293,6 @@ function pointerDown(e) {
     }
 }
 
-function translate(id, x, y) {
-    var dx = x-xOld;
-    var dy = y-yOld;
-    objectList[id].move(dx,dy);
-    xOld = x;
-    yOld = y;
-}
-
-function rotate(id, x, y) {
-    var obj = objectList[id];
-    var theta = Math.atan2(y-obj.midY(), x-obj.midX());
-    var pTheta = Math.atan2(yOld-obj.midY(), xOld-obj.midX());
-    obj.rotate(theta-pTheta);
-    xOld = x;
-    yOld = y;
-}
-
-function scale(id, x, y) {
-    var dx = x-xOld;
-    var dy = y-yOld;
-    if(shift) {
-        var obj = objectList[id];
-        var ratio = (yFirst-obj.midY())/(xFirst-obj.midX());
-        obj.xScale=xScaleOld;
-        obj.yScale=yScaleOld;
-        if(Math.abs(x-xFirst) < Math.abs(y-yFirst)) {
-            dx = x-xFirst;
-            dy = (x-xFirst)*ratio;
-        } else {
-            dx = (y-yFirst)/ratio;
-            dy = (y-yFirst);
-        }
-    }
-    objectList[id].scale(dx,dy);
-    xOld = x;
-    yOld = y;
-
-}
-
-function resetScale(id, x, y) {
-    var obj = objectList[id];
-    obj.xScale = 0;
-    obj.yScale = 0;
-    obj.scale(x-obj.midX(), y-obj.midY());
-}
-
 function pointerMove(e) {
     var c = transformCoordinates(e);
     var ctx = canvas.getContext('2d');
@@ -380,31 +313,7 @@ function pointerMove(e) {
                 }
                 break;
             case "select":
-                switch(dragMode) {
-                    case 'translate':
-                        translate(selectedId, x, y);
-                        break;
-                    case 'rotate':
-                        rotate(selectedId, x, y);
-                        break;
-                    case 'scale':
-                        var mx = objectList[selectedId].midX();
-                        var my = objectList[selectedId].midY();
-                        var pt = transformPoint(x-mx,y-my,
-                            mx, my,
-                            1, 1,
-                            objectList[selectedId].rotation);
-                        x = pt[0]; y = pt[1];
-
-                        if(shift == !e.shiftKey) {
-                            shift = !shift;
-                            if(!shift) {
-                                resetScale(selectedId, x, y);
-                            }
-                        }
-                        scale(selectedId, x, y);
-                        break;
-                }
+                applyTransform(selectedId, x, y, dragMode, e);
                 break;
             case "dropper":
                 var id = ctx.getImageData(x, y, 1, 1);
@@ -429,75 +338,7 @@ function pointerEnd(e) {
     }
 
     if(isDragging && (curTool == 'select')) {
-        //These seem like pointless variables, but if the're not defined, the  undo function will use global values
-        var id = selectedId;
-
-        switch(dragMode) {
-            case 'translate':
-                var dx = x-xFirst;
-                var dy = y-yFirst;
-
-                var newAct = {
-                    undo: function() {
-                        objectList[id].move(-dx, -dy);
-                    },
-                    redo: function() {
-                        objectList[id].move(dx, dy);
-                    }
-                };
-
-                addAction(newAct);
-                break;
-            case 'rotate':
-                var obj = objectList[id];
-                var dTheta  = Math.atan2(y-obj.midY(), x-obj.midX()) - Math.atan2(yFirst-obj.midY(), xFirst-obj.midX());
-
-                var newAct = {
-                    undo: function() {
-                        objectList[id].rotate(-dTheta);
-                    },
-                    redo: function() {
-                        objectList[id].rotate(dTheta);
-                    }
-                };
-
-                addAction(newAct);
-                break;
-            case 'scale':
-                var obj = objectList[id];
-                var mx = obj.midX();
-                var my = obj.midY();
-                var pt = transformPoint(x-mx,y-my,
-                    mx, my,
-                    1, 1,
-                    obj.rotation);
-                x = pt[0]; y = pt[1];
-
-                var dx = x-xFirst;
-                var dy = y-yFirst;
-                if(shift) {
-                    var ratio = (yFirst-obj.midY())/(xFirst-obj.midX());
-                    if(Math.abs(x-xFirst) < Math.abs(y-yFirst)) {
-                        dx = x-xFirst;
-                        dy = (x-xFirst)*ratio;
-                    } else {
-                        dx = (y-yFirst)/ratio;
-                        dy = (y-yFirst);
-                    }
-                }
-
-                var newAct = {
-                    undo: function() {
-                        objectList[id].scale(-dx, -dy);
-                    },
-                    redo: function() {
-                        objectList[id].scale(dx, dy);
-                    }
-                };
-
-                addAction(newAct);
-                break;
-        }
+        endTransform(selectedId, x, y, dragMode);
     }
     
     isDragging = false;
