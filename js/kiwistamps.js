7056ec0f62fbcf5dfc7f69660ebdea687c443479
Johnny Zabonik <jzabonik@mymail.mines.edu>
2013-05-31 14:55:27 -0600

    put the layer icons on textboxes

--- js/kiwistamps.js
+++ js/kiwistamps.js
@@ -302,6 +302,24 @@ function createTextBalloon(dObj) {
 
             var rotateIcon = document.getElementById('rotate_icon');
             ctx.drawImage(rotateIcon, rightCorner[0]-32, rightCorner[1]-32);
+			
+		var leftBottom = transformPoint(
+            0, this.height,
+            this.pts[0], this.pts[1],
+            this.xScale, this.yScale,
+            -this.rotation );
+
+            var downIcon = document.getElementById('arrow_down');
+            ctx.drawImage(downIcon, leftBottom[0]-16, leftBottom[1]-16);
+
+        var rightBottom = transformPoint(
+            this.width, this.height,
+            this.pts[0], this.pts[1],
+            this.xScale, this.yScale,
+            -this.rotation );
+
+            var upIcon = document.getElementById('arrow_up');
+            ctx.drawImage(upIcon, rightBottom[0]-16, rightBottom[1]-16);
     }
 
     // Rotate this stamp by dr radians.
@@ -327,8 +345,21 @@ function createTextBalloon(dObj) {
             this.pts[0], this.pts[1],
             this.xScale, this.yScale,
             -this.rotation );
+		var leftBottom = transformPoint(
+            0, this.height,
+            this.pts[0], this.pts[1],
+            this.xScale, this.yScale,
+            -this.rotation );
+        var rightBottom = transformPoint(
+            this.width, this.height,
+            this.pts[0], this.pts[1],
+            this.xScale, this.yScale,
+            -this.rotation );
+
         if(distance([x,y],[leftCorner[0], leftCorner[1]]) < 32) { return 'scale'; }
         else if(distance([x,y],[rightCorner[0], rightCorner[1]]) < 32) { return 'rotate'; }
+		else if(distance([x,y],[rightBottom[0], rightBottom[1]]) < 16) { return 'layerUp'; }
+		else if(distance([x,y],[leftBottom[0], leftBottom[1]]) < 16) { return 'layerDown'; } 
         else { return false; }
     }
 
