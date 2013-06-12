bee3eb0efe2f570beb408abf546210cfdc86ee16
max <max@Lyla.(none)>
2013-06-12 11:25:43 -0600

    ...

--- js/kiwitext.js
+++ js/kiwitext.js
@@ -162,7 +162,7 @@ function createTextBalloon(dObj) {
                 showKeyboard();
                 return true;
             }
-            canvas.focus();
+            window.focus();
             return false;
         }
         else{
@@ -170,7 +170,7 @@ function createTextBalloon(dObj) {
                 showKeyboard();
                 return true;
             }
-            canvas.focus();
+            window.focus();
             return false;
         }
     };
