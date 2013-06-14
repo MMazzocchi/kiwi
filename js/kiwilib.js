f43e944cf0ab325dc1784102e40912f5a32b14cf
max <max@Lyla.(none)>
2013-06-14 08:58:37 -0600

    Testing mobile

--- js/kiwilib.js
+++ js/kiwilib.js
@@ -110,9 +110,9 @@ function refreshCanvas() {
 
         switch(orientation) {
             case 0:
-                ctx.canvas.width = height;
-                ctx.canvas.height = width;
-                ctx.fillRect(0,0,window.innerWidth,window.innerHeight);
+                ctx.canvas.width = width;
+                ctx.canvas.height = height;
+                ctx.fillRect(0,0,width,height);
                 tx=0; ty=0; 
                 break;
             case -90:
